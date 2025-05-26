cf985aaafc6e0533e4e348f7eae438ac0f94f364
r-yvan <yvankiliye.rubuto@gmail.com>
2025-05-26 10:22:05 +0200

    Comit

---
+++ src/main/java/com/healify/HealifyApplication.java
@@ -0,0 +1,13 @@
+package com.healify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealifyApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(HealifyApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/com/healify/HealifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealifyApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
